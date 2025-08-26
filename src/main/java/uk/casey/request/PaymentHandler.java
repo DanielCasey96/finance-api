845bcd26f41438589054c2fac7e4092c1eec06a3
Dan Casey <dcasey@gmail.com>
2025-08-26 20:51:17 +0100

    add 1 header validation

--- src/main/java/uk/casey/request/PaymentHandler.java
+++ src/main/java/uk/casey/request/PaymentHandler.java
@@ -9,18 +9,30 @@ import java.io.InputStream;
 public class PaymentHandler implements HttpHandler {
 
     public void handle(HttpExchange exchange) throws IOException {
+        PaymentService paymentService = new PaymentService();
+
         if ("POST".equals(exchange.getRequestMethod())) {
+
+            //Request Header Validation
+            String ContentType = exchange.getRequestHeaders().getFirst("Content-Type");
+            if (ContentType == null || !ContentType.equals("application/json")) {
+                exchange.sendResponseHeaders(400, -1); // Bad Request
+                System.out.println("Missing or invalid Content-Type header");
+                return;
+            }
+
+            //Request Body Validation
             InputStream requestBody = exchange.getRequestBody();
             StringBuilder body = new StringBuilder();
             int ch;
             while ((ch = requestBody.read()) != -1) {
                 body.append((char) ch);
             }
-            PaymentService paymentService = new PaymentService();
             if (!paymentService.isBodyValid(body.toString())) {
                 exchange.sendResponseHeaders(400, -1); // Bad Request
                 return;
             }
+
             System.out.println("Received payment data: " + body.toString());
             String response = "Payment processed";
             exchange.sendResponseHeaders(200, response.length());
